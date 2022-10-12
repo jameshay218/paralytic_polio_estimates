2afc47fec5ef53ef7e15ebf1a64a5b5cc72ba9cc
hillof3oaks <bushmanmary@gmail.com>
2022-10-11 21:59:20 -0400

    used updated Generate_priors code with four coverage parameters (IPV age 2, IPV age 5 premandate, IPV age 5 postmandate, OPV age 2). Removed parameters for NYC scenario as this is not currently in use. Rockland low coverage scenario: coverage levels = 30%, 50%, 70%, 90% Rockland high coverage scenario: coverage levels = 60%, 90%, 95%, 90%

--- prior_derivation_scripts/polio model priors.R
+++ prior_derivation_scripts/polio model priors.R
@@ -4,10 +4,11 @@ source("Generate priors.R")
 
 ############################
 
-rockland_low <- generate_priors(specify_counties = "Rockland", 
-                                coverage_1 = 0.4, 
-                                coverage_2 = 0.6, 
-                                coverage_3 = 0.95)
+rockland_low <- generate_priors(specify_counties = "Rockland",
+                                coverage_1 = 0.3, 
+                                coverage_2 = 0.5, 
+                                coverage_3 = 0.7,
+                                coverage_4 = 0.9)
 
 pars_rockland_low <- data.frame(matrix(nrow=7,ncol=4,dimnames=list(NULL,c("model parameter","distribution","par1","par2"))))
 
@@ -33,10 +34,11 @@ pars_rockland_low[,3]/(pars_rockland_low[,3]+pars_rockland_low[,4])
 
 ############################
 
-rockland_high <- generate_priors(specify_counties = "Rockland", 
+rockland_high <- generate_priors(specify_counties = "Rockland",
                                 coverage_1 = 0.6, 
                                 coverage_2 = 0.9, 
-                                coverage_3 = 0.95)
+                                coverage_3 = 0.95,
+                                coverage_4 = 0.9)
 
 pars_rockland_high <- data.frame(matrix(nrow=7,ncol=4,dimnames=list(NULL,c("model parameter","distribution","par1","par2"))))
 
@@ -58,38 +60,11 @@ pars_rockland_high[,2] <- c("beta", #1
 
 for(i in 1:7){pars_rockland_high[i,3:4] <- unname(fitdist(rockland_high[,i], distr=pars_rockland_high[i,2], method="mge", gof="CvM")$estimate)}
 
-################################
+#############################
 
-nyc <- generate_priors(specify_counties = c("Bronx", "Brooklyn", "Manhattan", "Queens", "Staten"), 
-coverage_1 = 0.9, 
-coverage_2 = 0.95, 
-coverage_3 = 0.95)
+scenario <- rep(c("rockland_low_coverage", "rockland_high_coverage"), rep(7,2))
 
-pars_nyc <- data.frame(matrix(nrow=7,ncol=4,dimnames=list(NULL,c("model parameter","distribution","par1","par2"))))
-
-pars_nyc[,1] <- c("prop_susceptible","prop_refractory","relative_infectiousness","susceptible_generation_interval_shape","susceptible_generation_interval_rate","partial_generation_interval_shape","partial_generation_interval_rate")
-
-x<-nyc[,1] 
-hist(x,breaks=100,freq=F)
-fit <- fitdist(x,"beta","mge",gof="CvM")$estimate
-curve(dbeta(x, fit[1],fit[2]),from=min(x),to=max(x),add=T, lwd=2)
-
-pars_nyc[,2] <- c("beta", #1
-                            "beta", #2
-                            "beta", #3
-                            "norm", #4
-                            "beta", #5
-                            "norm", #6
-                            "beta" #7
-)
-
-for(i in 1:7){pars_nyc[i,3:4] <- unname(fitdist(nyc[,i], distr=pars_nyc[i,2], method="mge", gof="CvM")$estimate)}
-
-########################
-
-scenario <- rep(c("rockland_low_coverage", "rockland_high_coverage", "NYC"), rep(7,3))
-
-priors <- cbind(scenario, rbind(pars_rockland_low, pars_rockland_high, pars_nyc))
+priors <- cbind(scenario, rbind(pars_rockland_low, pars_rockland_high))
 
 priors
 
